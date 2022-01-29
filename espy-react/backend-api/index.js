583e48c638448266d8f365ebdf5b5a1f768d0ff8
dwskme <dwskme@gmail.com>
2022-01-29 19:11:29 +0545

    Added routed and user auth

--- espy-react/backend-api/index.js
+++ espy-react/backend-api/index.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+    })
+    .then(() => console.log("DB Connection Successfull"))
+    .catch((err) => {
+        console.error(err);
+    });
 
-
+app.use(express.json());
 app.listen(8800, () => {
-    console.log("Server Running");
-})
+    console.log("Backend server is running!");
+});
