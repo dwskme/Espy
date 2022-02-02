2fbb5c57b88b099775f104633eb779b58088e4be
dwskme <dwskme@gmail.com>
2022-02-02 13:54:09 +0545

    Added delete method

--- espy-react/backend-api/routes/users.js
+++ espy-react/backend-api/routes/users.js
@@ -4,6 +4,7 @@ const CryptoJS = require("crypto-js");
 const verify = require("../verifyToken");
 
 // Update method for User.
+// FIXME: Server throws error if token is removed...
 router.put("/:id", verify, async (req, res) => {
   if (req.user.id === req.params.id || req.user.isAdmin) {
     if (req.body.password) {
@@ -26,7 +27,24 @@ router.put("/:id", verify, async (req, res) => {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You can update only your account!");
+    res.status(403).json("You can only update your account!");
   }
 });
+
+
+// Delete Method
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User deleted successfully!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can only delete your account!");
+  }
+});
+
+
 module.exports = router;
