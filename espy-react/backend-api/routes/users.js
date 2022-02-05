a3ec22bee30f5764787abe3fc370c7da3bdd738f
dwskme <dwskme@gmail.com>
2022-02-05 22:25:57 +0545

    Added user stats and get method

--- espy-react/backend-api/routes/users.js
+++ espy-react/backend-api/routes/users.js
@@ -48,35 +48,74 @@ router.delete("/:id", verify, async (req, res) => {
 
 // GET Method
 
-router.get("/find/:id", verify, async (req, res) => {
+router.get("/find/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    // Hiding Password while returning
-    const {
-      password,
-      ...info
-    } = user._doc;
-
-    res.status(200).json(user);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// GET All
-
+//GET ALL
 router.get("/", verify, async (req, res) => {
   const query = req.query.new;
   if (req.user.isAdmin) {
     try {
-      const users = query ? await User.find().limit(10): await User.find();
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(5)
+        : await User.find();
       res.status(200).json(users);
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You are not allowed to perform this request");
+    res.status(403).json("You are not allowed to see all users!");
   }
 });
+
+//GET USER STATS
+router.get("/stats", async (req, res) => {
+  const today = new Date();
+  // getting last year
+  const lastYear = today.setFullYear(today.setFullYear() - 1);
+    // 12 months of year
+    const month = [  
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+  
+
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
