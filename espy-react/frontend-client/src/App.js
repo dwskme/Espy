468bcc0f6e01cd35537b5430224a7d57c0489be9
dwskme <dwskme@gmail.com>
2022-02-27 07:58:12 +0545

    Minor Changes:

--- espy-react/frontend-client/src/App.js
+++ espy-react/frontend-client/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './styles/style.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import NavBar from './components/NavBar';
 function App() {
   return (
-    <div className='flex  items-center justify-center min-h-screen py-2 bg-gray-100'>
-
-      <Login />
-    {/* <Register/> */}
-    
+    <div>
+      <NavBar/>
+      <Login/>
+      <Register/>
     </div>
   );
 }
