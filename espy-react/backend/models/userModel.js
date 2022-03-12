2621c2470026ddb3ba0d62e9bc6745a5b72a02cf
dwskme <dwskme@gmail.com>
2022-03-12 21:03:41 +0545

    Adding Login, JWT token, cookies

--- espy-react/backend/models/userModel.js
+++ espy-react/backend/models/userModel.js
@@ -1,52 +1,71 @@
-const mongoose = require('mongoose');	
+const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs');	
+const bcrypt = require('bcryptjs');
+const JWT = require('jsonwebtoken');
+
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, "Please enter your name"],
-        maxLength:[30, "Name must be less than 30 characters"],
-        minLength:[4, "Name must be at least 4 characters"],
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        maxLength: [30, "Name must be less than 30 characters"],
+        minLength: [4, "Name must be at least 4 characters"],
     },
-    email:{
-        type:String,
-        required:[true, "Please enter your email"],
-        unique:true,
-        validate:[validator.isEmail,"Please enter a valid email"]
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        validate: [validator.isEmail, "Please enter a valid email"]
     },
-    password:{
-        type:String,
-        required:[true, "Please enter your password"],
-        minLength:[8, "Password must be at least 8 characters"],
-        maxLength:[16, "Password must be less than 16 characters"],
-        select:false
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minLength: [8, "Password must be at least 8 characters"],
+        maxLength: [16, "Password must be less than 16 characters"],
+        select: false
     },
-    avatar:{
-        public_id:{
-            type:String,
-            required:true,
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
         },
-        url:{
-            type:String,
-            required:true,
+        url: {
+            type: String,
+            required: true,
         }
     },
-    role:{
-        type:String,
-        default:"user",	
+    role: {
+        type: String,
+        default: "user",
     },
-    resetPasswordToken:String,
-    resetPasswordExpire:Date,
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
 });
 
-userSchema.pre("save",async function(next)
-{
-    if(!this.isModified("password")){
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
         next();
     }
-    this.password = bcrypt.hash(this.password,10);
-})
+    this.password = await bcrypt.hash(this.password, 10);
+});
 
 
-module.exports = mongoose.model('User', userSchema); 
+// JWS Token
+userSchema.methods.getJWTToken = function () {
+    return JWT.sign({
+        id: this._id
+    }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+};
+
+// Compare Password
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+
+
+
+module.exports = mongoose.model('User', userSchema);
