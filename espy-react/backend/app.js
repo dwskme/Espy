f32bc68db9656daeda754a378e48ebe31fdf916d
dwskme <dwskme@gmail.com>
2022-03-12 20:03:25 +0545

    User route, model controller added

--- espy-react/backend/app.js
+++ espy-react/backend/app.js
@@ -6,7 +6,9 @@ app.use(express.json());
 
 // Routes Imports
 const movies =require('./routes/moviesRoute'); 
+const user = require('./routes/userRoute');	
 app.use('/api/v1', movies);	
+app.use('/api/v1', user);	
 
 
 // Middleware for Errors
---
+++ espy-react/backend/models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');	
+const validator = require('validator');
+
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "Please enter your name"],
+        maxLength:[30, "Name must be less than 30 characters"],
+        minLength:[4, "Name must be at least 4 characters"],
+    },
+    email:{
+        type:String,
+        required:[true, "Please enter your email"],
+        unique:true,
+        validate:[validator.isEmail,"Please enter a valid email"]
+    },
+    password:{
+        type:String,
+        required:[true, "Please enter your password"],
+        minLength:[8, "Password must be at least 8 characters"],
+        maxLength:[16, "Password must be less than 16 characters"],
+        select:false
+    },
+    avatar:{
+        public_id:{
+            type:String,
+            required:true,
+        },
+        url:{
+            type:String,
+            required:true,
+        }
+    },
+    role:{
+        type:String,
+        default:"user",	
+    },
+    resetPasswordToken:String,
+    resetPasswordExpire:Date,
+
+});
+
+module.exports = mongoose.model('User', userSchema); 
