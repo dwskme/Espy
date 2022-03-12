01ba63735b59b8d6dd6ab2ee70e6aced76ee7ada
dwskme <dwskme@gmail.com>
2022-03-12 17:19:50 +0545

    ID Cast Error Handling

--- espy-react/backend/middleware/error.js
+++ espy-react/backend/middleware/error.js
@@ -5,6 +5,13 @@ module.exports = (err, req, res, next)=>{
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Something went wrong Internally';
 
+
+    // Wrong MongoDB ID error
+    if(err.name === 'CastError'){	
+        const message = `Resource not found with id ${err.path}`;
+        err = new ErrorHandler(message, 400);	 
+    }
+
     res.status(err.statusCode).json({
         success:false,
         message: err.message,
