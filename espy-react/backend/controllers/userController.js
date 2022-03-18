19e9d502741453e2e355366cdc76125438ea270d
dwskme <dwskme@gmail.com>
2022-03-18 16:04:12 +0545

    Fix await missing

--- espy-react/backend/controllers/userController.js
+++ espy-react/backend/controllers/userController.js
@@ -164,13 +164,14 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
     const newUserData = {
         name:req.body.name,
         email:req.body.email,
-    }
+    };
 
-    const user = User.findByIdAndUpdate(req.user.id, newUserData, {
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
         new:true,
         runValidators:true,
         useFindAndModify:false,
     });
+    
 res.status(200).json({
     success:true,
 });
