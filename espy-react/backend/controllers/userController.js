f3db6ba466ad93ef38f82c341e4b0cdda6429486
dwskme <dwskme@gmail.com>
2022-03-18 14:23:50 +0545

    Added logout func

--- espy-react/backend/controllers/userController.js
+++ espy-react/backend/controllers/userController.js
@@ -54,3 +54,18 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     }
     sendToken(user,200,res);
 });
+
+
+// LogOut User
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    })
+    
+    res.status(200).json({
+        success:true,
+        message:"Logged Out Successfully"
+    })
+
+})
