cbe8c9878f45acdad199e7dd2d0c7027eeaa7945
dwskme <dwskme@gmail.com>
2022-03-30 21:45:23 +0545

    Handling Empty form error

--- espy-react/frontend/src/pages/Register.js
+++ espy-react/frontend/src/pages/Register.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react'
-// import { FaRegEnvelope, FaUser } from "react-icons/fa";
-// import { MdLockOutline } from "react-icons/md";
-import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-
+import  placeholder1  from "../img/placeholder1.png";
 
 const Register = () => {
 
-    const navigate = useNavigate();
     const [name, setName] = useState('');
     const [gender, setGender] = useState('Male');
     const [age, setAge] = useState('');
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name, email, password, gender, age);
-        // console.log(fullName, email,);
+        if((name.length===0) && (email.length===0 && (password.length===0) ))
+        {
+            toast.error("Empty Feilds not allowed");
+        }
+        else if (name.length === 0 ){
+            toast.error("Empty Name Feild");
+        }
+        else if (email.length === 0 ){
+            toast.error("Empty Email Feild");
+        }
+        else if (password.length === 0 ){
+            toast.error("Empty Password Feild Feild");
+        }
+        else{}
+        if(password.length < 8){
+            toast.error("Password must be atleast 8 Characters.")
+        }else{}
         axios.post('/api/v1/register', { name, email, password, gender, age }).then(function (result) {
-            console.log(result.data);
+            // While regestering adding token to localStorage.
             localStorage.setItem('token', result.data.token);
             if (result.data.success) {
                 toast.success("Registered Successfully.", { position: toast.POSITION_TOP_RIGHT })
@@ -40,21 +50,20 @@ const Register = () => {
                 <div className="container-fluid h-custom">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col-md-9 col-lg-6 col-xl-5">
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid"
-                                alt="" />
+                            <img src={placeholder1} className="img-fluid"
+                                alt="PlaceHolderImage." />
                         </div>
                         <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                             <h3 className='my-5'>Register</h3>
-
                             <form>
                                 <div className="form-outline mb-4">
-                                    <label className="form-label" htmlFor="form3Example3">Full Name</label>
+                                    <label className="form-label" >Full Name</label>
                                     <input onChange={(e) => setName(e.target.value)} type="text" id="form3Example3" className="form-control form-control-lg"
                                         placeholder="Enter your name" />
                                 </div>
                                 <div className='row'>
                                     <div className='form-outline mb-4 col-md-6'>
-                                        <label className="form-label" htmlFor="form3Example3">Gender</label>
+                                        <label className="form-label" >Gender</label>
                                         <select onChange={(e) => { setGender(e.target.value) }} className="form-select form-select-lg" aria-label="Default select example">
                                             <option selected value="Male">Male</option>
                                             <option value="Female">Female</option>
@@ -62,7 +71,7 @@ const Register = () => {
                                     </div>
 
                                     <div className="form-outline mb-4 col-md-6">
-                                        <label className="form-label" htmlFor="form3Example3">Age</label>
+                                        <label className="form-label" >Age</label>
 
                                         <input onChange={(e) => setAge(e.target.value)} type="number" id="form3Example3" className="form-control form-control-lg"
                                             placeholder="Enter Age" />
@@ -71,7 +80,7 @@ const Register = () => {
 
 
                                 <div className="form-outline mb-4">
-                                    <label className="form-label" htmlFor="form3Example3">Email address</label>
+                                    <label className="form-label" >Email address</label>
                                     <input onChange={(e) => setEmail(e.target.value)} type="email" id="form3Example3" className="form-control form-control-lg"
                                         placeholder="Enter a valid email address" />
                                 </div>
