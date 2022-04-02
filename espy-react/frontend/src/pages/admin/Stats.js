ba609c28af6f49d0df59dd6bb16452e703130d1f
dwskme <dwskme@gmail.com>
2022-04-02 11:55:35 +0545

    Showing Stats and Similar Movies

--- espy-react/frontend/src/pages/admin/Stats.js
+++ espy-react/frontend/src/pages/admin/Stats.js
@@ -2,19 +2,29 @@ import React from 'react'
 import { useState,useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
-
+import {API_KEY} from '../../config';
 
 
 const Stats = (props)=>  {
     const navigate = useNavigate();
     const [topData, setTopData] = useState();
+    const [movieData, setMovieData] = useState();
 
     useEffect(() => {
         axios.get("/api/v1/get-stats").then(function (result) {
             console.log("result: "+ result.data.movies[0].id);
             setTopData(result.data?.movies);
         })
-        
+        const id = function(){
+            topData.map((data,index)=>{
+            
+            })
+        }
+
+        axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`).then(function(result){
+            console.log("Movie Details: "+ result);
+            setMovieData(result);
+        })
     }, [])
 
 
@@ -22,13 +32,15 @@ const Stats = (props)=>  {
 return (
     <>
     { 
-    topData?.map((movie, index)=>{
+    movieData?.map((movie, index)=>{
         return (
-        <h1>
-        {
-            movie?.id
-        }    
-        </h1>)
+        <>
+            <h1>
+            {
+                movie?.id
+            }    
+            </h1>
+        </>)
     })
     }
     </>
